6b6873e6fb18d3d881a26f6bf3891bf389c78f1a
arthurloizeau7389 <arthurloizeau7389@penguin>
2024-03-20 08:55:06 +0100

    MaJ du next de Acteur

--- src/abstraction/eq3Producteur3/Producteur3Acteur.java
+++ src/abstraction/eq3Producteur3/Producteur3Acteur.java
@@ -13,6 +13,7 @@ import abstraction.eqXRomu.produits.IProduit;
 public class Producteur3Acteur implements IActeur {
 	
 	protected int cryptogramme;
+	protected Journal journal;
 
 	public Producteur3Acteur() {
 	}
@@ -33,6 +34,7 @@ public class Producteur3Acteur implements IActeur {
 	////////////////////////////////////////////////////////
 
 	public void next() {
+		this.journal.ajouter("etape="+Filiere.LA_FILIERE.getEtape());
 	}
 
 	public Color getColor() {// NE PAS MODIFIER
@@ -40,7 +42,7 @@ public class Producteur3Acteur implements IActeur {
 	}
 
 	public String getDescription() {
-		return "Bla bla bla";
+		return "tiCao - Producteur 3";
 	}
 
 	// Renvoie les indicateurs
