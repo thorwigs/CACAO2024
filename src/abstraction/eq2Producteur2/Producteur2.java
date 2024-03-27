3d93a89840919b16f3e1c1a4908e82614ab6aa4e
quentindenis <quentindenis@dhcp-133-244.mde.emn.fr>
2024-03-27 12:17:46 +0100

    Changement 2

--- src/abstraction/eq2Producteur2/Producteur2.java
+++ src/abstraction/eq2Producteur2/Producteur2.java
@@ -1,6 +1,6 @@
 package abstraction.eq2Producteur2;
 
-public class Producteur2 extends Producteur2AcheteurCCadre {
+public class Producteur2 extends Producteur2VendeurCCadre {
 	public Producteur2() {
 		super();
 	}
