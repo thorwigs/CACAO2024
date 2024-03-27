c0bf327a605e4cef57fa100bc3bbb28282534a61
quentindenis <quentindenis@dhcp-133-244.mde.emn.fr>
2024-03-27 12:02:30 +0100

    Stock 2

--- src/abstraction/eq2Producteur2/Producteur2_Stocks.java
+++ src/abstraction/eq2Producteur2/Producteur2_Stocks.java
@@ -2,8 +2,12 @@ package abstraction.eq2Producteur2;
 
 public class Producteur2_Stocks extends Producteur2Acteur {
 	
-	private static final int SEUIL = 0;
-	//private final int PRIX_STOCK_TONNE = 0;
+	private static final int SEUIL = 0; 
+	//seuil max de la production stockee (voir ce qu'on fait du reste: vente, poubelle, produit moins, ...)
+	//déterminer ce qu'on fait en fonction de comment on est proche du seuil max
+	
+	//private final int PRIX_STOCK_TONNE = 0; récupérable via la filière
+	
 	private static final float DELAI_HQ_MQ = 0;
 	private static final float DELAI_MQ_BQ = 0;
 	
@@ -12,7 +16,43 @@ public class Producteur2_Stocks extends Producteur2Acteur {
 	private int quantite_stockee_bq;
 	
 	//méthode pour déterminer passage de haute_qualite à moyenne et de moyenne à basse
-
+	//méthode update pour mettre à jour à chaque tour le stock
 	
+	
+	public int getQuantite_stockee_hq() {
+		return quantite_stockee_hq;
+	}
+	
+	public void setQuantite_stockee_hq(int quantite_stockee_hq) {
+		this.quantite_stockee_hq = quantite_stockee_hq;
+	}
+	
+	public int getQuantite_stockee_mq() {
+		return quantite_stockee_mq;
+	}
+	
+	public void setQuantite_stockee_mq(int quantite_stockee_mq) {
+		this.quantite_stockee_mq = quantite_stockee_mq;
+	}
+	
+	public int getQuantite_stockee_bq() {
+		return quantite_stockee_bq;
+	}
+	
+	public void setQuantite_stockee_bq(int quantite_stockee_bq) {
+		this.quantite_stockee_bq = quantite_stockee_bq;
+	}
+	
+	public static int getSeuil() {
+		return SEUIL;
+	}
+	
+	public static float getDelaiHqMq() {
+		return DELAI_HQ_MQ;
+	}
+	
+	public static float getDelaiMqBq() {
+		return DELAI_MQ_BQ;
+	}
 
 }
