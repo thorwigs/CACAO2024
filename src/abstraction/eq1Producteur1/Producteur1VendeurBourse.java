9b1f5945058a6b18b4270f0b5b4af2c00e0ab312
HP PRO <HP PRO@172.16.135.173>
2024-03-27 11:41:11 +0000

    fonction offre ..

--- src/abstraction/eq1Producteur1/Producteur1VendeurBourse.java
+++ src/abstraction/eq1Producteur1/Producteur1VendeurBourse.java
@@ -1,13 +1,48 @@
 package abstraction.eq1Producteur1;
 
 import abstraction.eqXRomu.bourseCacao.IVendeurBourse;
+import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.general.Journal;
 import abstraction.eqXRomu.produits.Feve;
+import abstraction.eqXRomu.produits.Gamme;
+import abstraction.eqXRomu.produits.IProduit;
 
 public class Producteur1VendeurBourse extends Producteur1Acteur implements  IVendeurBourse {
-
+	public double  PrixSeuilHQ ;
+	public double  PrixSeuilBQ ;
+	public double  PrixSeuilMQ ;
+	private Journal journalBourse;
+	
+	
+// Fatima-ezzahra
 	@Override
 	public double offre(Feve f, double cours) {
 		// TODO Auto-generated method stub
+		
+	   double quantiteEnT = this.getQuantiteEnStock(  f ,   cryptogramme);
+	   if (quantiteEnT!=0) {
+		   
+	  
+		if (f.getGamme()==Gamme.MQ) {
+			if(cours>=PrixSeuilMQ) {
+				journalBourse.ajouter(Filiere.LA_FILIERE.getEtape()+" : je met en vente "+quantiteEnT+" T de "+f);
+				return quantiteEnT;
+			}
+		}
+		if (f.getGamme()==Gamme.HQ) {
+			if(cours>=PrixSeuilHQ) {
+				journalBourse.ajouter(Filiere.LA_FILIERE.getEtape()+" : je met en vente "+quantiteEnT+" T de "+f);
+				return quantiteEnT;
+			}
+		}
+		if (f.getGamme()==Gamme.HQ) {
+			if(cours>=PrixSeuilHQ) {
+				journalBourse.ajouter(Filiere.LA_FILIERE.getEtape()+" : je met en vente "+quantiteEnT+" T de "+f);
+				return quantiteEnT;
+			}
+		}
+	   }
+		
 		return 0;
 	}
 
