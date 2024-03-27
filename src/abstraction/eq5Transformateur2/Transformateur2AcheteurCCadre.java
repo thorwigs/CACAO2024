d0469d88d321bcd3f1fe1f23448fdde95983629f
Erwann <erwannbarrelebiavant@dhcp-134-12.mde.emn.fr>
2024-03-27 11:06:04 +0100

    CCadre

--- src/abstraction/eq5Transformateur2/Transformateur2AcheteurCCadre.java
+++ src/abstraction/eq5Transformateur2/Transformateur2AcheteurCCadre.java
@@ -1,14 +1,50 @@
 package abstraction.eq5Transformateur2;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import abstraction.eqXRomu.contratsCadres.Echeancier;
 import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
 import abstraction.eqXRomu.contratsCadres.IAcheteurContratCadre;
+import abstraction.eqXRomu.contratsCadres.SuperviseurVentesContratCadre;
+import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.general.Journal;
+import abstraction.eqXRomu.produits.Feve;
 import abstraction.eqXRomu.produits.IProduit;
 
 public class Transformateur2AcheteurCCadre extends Transformateur2Acteur implements IAcheteurContratCadre {
-
+	private SuperviseurVentesContratCadre supCC;
+	private List<ExemplaireContratCadre> contratsEnCours;
+	private List<ExemplaireContratCadre> contratsTermines;
+	protected Journal journalCC;
+	
+	
+	// Constructor //
+	public Transformateur2AcheteurCCadre() {
+		super();
+		this.contratsEnCours = new LinkedList<ExemplaireContratCadre>();
+		this.contratsTermines = new LinkedList<ExemplaireContratCadre>();
+		this.journalCC = new Journal(this.getNom()+" journal CC", this);
+	}
+	
+	// Initialise le contrat //
+	public void initialiser() {
+		super.initialiser();
+		this.supCC = (SuperviseurVentesContratCadre)(Filiere.LA_FILIERE.getActeur("Sup.CCadre"));
+	}
+	
+	// 
+	public double restantDu(Feve f) {
+		double res=0;
+		for (ExemplaireContratCadre c : this.contratsEnCours) {
+			if (c.getProduit().equals(f)) {
+				res+=c.getQuantiteRestantALivrer();
+			}
+		}
+		return res;
+	}
+	
 	public boolean achete(IProduit produit) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
