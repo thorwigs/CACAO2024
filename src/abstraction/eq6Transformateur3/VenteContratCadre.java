46737bc1352db2ff5ba0f7804c06803117856116
Thomas <Thomas@172.16.136.177>
2024-03-27 11:23:11 +0100

    Merge branch 'main' of https://github.com/CACAO2024/CACAO2024.git into main

---
+++ src/abstraction/eq6Transformateur3/VenteContratCadre.java
@@ -0,0 +1,46 @@
+package abstraction.eq6Transformateur3;
+
+import abstraction.eqXRomu.contratsCadres.Echeancier;
+import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eqXRomu.contratsCadres.IVendeurContratCadre;
+import abstraction.eqXRomu.produits.IProduit;
+
+public class VenteContratCadre extends Transformateur3Acteur implements IVendeurContratCadre{
+
+	@Override
+	public boolean vend(IProduit produit) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Echeancier contrePropositionDuVendeur(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double propositionPrix(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double contrePropositionPrixVendeur(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void notificationNouveauContratCadre(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double livrer(IProduit produit, double quantite, ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
