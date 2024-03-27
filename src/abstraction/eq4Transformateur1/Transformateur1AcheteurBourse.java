62d826ace21f9cb0e49dcdd18451a6b53b868bc9
noemiegrosset <grosset.noemie@gmail.com>
2024-03-27 10:28:46 +0100

    début bourse

--- src/abstraction/eq4Transformateur1/Transformateur1AcheteurBourse.java
+++ src/abstraction/eq4Transformateur1/Transformateur1AcheteurBourse.java
@@ -1,5 +1,26 @@
 package abstraction.eq4Transformateur1;
 
-public class Transformateur1AcheteurBourse extends Transformateur1Vendeur {
+import abstraction.eqXRomu.bourseCacao.IAcheteurBourse;
+import abstraction.eqXRomu.produits.Feve;
+
+public class Transformateur1AcheteurBourse extends Transformateur1Vendeur implements IAcheteurBourse {
+
+	@Override
+	public double demande(Feve f, double cours) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void notificationAchat(Feve f, double quantiteEnT, double coursEnEuroParT) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notificationBlackList(int dureeEnStep) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
