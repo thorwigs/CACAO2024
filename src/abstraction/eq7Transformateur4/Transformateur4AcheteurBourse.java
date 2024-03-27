9dcc68380f810b44eaf1e15f6542367d13e6721f
Eliott <Eliott@172.16.131.229>
2024-03-27 12:13:19 +0100

    Merge branch 'main' of https://github.com/Pierrick289/CACAO2024 into main

--- src/abstraction/eq7Transformateur4/Transformateur4AcheteurBourse.java
+++ src/abstraction/eq7Transformateur4/Transformateur4AcheteurBourse.java
@@ -8,20 +8,28 @@ public class Transformateur4AcheteurBourse extends Transformateur4Acteur impleme
 
 	@Override
 	public double demande(Feve f, double cours) {
+		return 20;
 		// TODO Auto-generated method stub
-		return 0;
 	}
 
 	@Override
 	public void notificationAchat(Feve f, double quantiteEnT, double coursEnEuroParT) {
+		this.stockFeves.put(f, this.stockFeves.get(f)+quantiteEnT);
+		this.totalStocksFeves.ajouter(this, quantiteEnT, cryptogramme);
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void notificationBlackList(int dureeEnStep) {
+		
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public void next() {
+		super.next();
+	}
+	
 
 }
