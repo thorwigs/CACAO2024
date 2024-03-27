496e51b33073e76b87a2e2331ed50f7cfaa3ead3
youss <youss@172.16.133.237>
2024-03-27 11:24:20 +0100

    youssef venteAuxEncheres

--- src/abstraction/eq1Producteur1/Producteur1VendeurAuxEncheres.java
+++ src/abstraction/eq1Producteur1/Producteur1VendeurAuxEncheres.java
@@ -4,16 +4,12 @@ import java.util.List;
 
 import abstraction.eqXRomu.encheres.Enchere;
 import abstraction.eqXRomu.encheres.IVendeurAuxEncheres;
+import abstraction.eqXRomu.encheres.SuperviseurVentesAuxEncheres;
+import abstraction.eqXRomu.produits.Chocolat;
 /**************************	
 	YOUSSEF BEN ABDELJELIL		**********************/
 
-public class Producteur1VendeurAuxEncheres extends Producteur1VendeurCCadre implements IVendeurAuxEncheres{
 
-	@Override
-	public Enchere choisir(List<Enchere> propositions) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 			
 
-}
+
