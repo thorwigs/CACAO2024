99df5c3718025bc7eba822505205adf20e9eb6e5
yanni <yanni@172.16.132.14>
2024-03-27 10:27:55 +0100

    création vendeur ccadre

---
+++ src/abstraction/eq4Transformateur1/Transformateur1VendeurCCadre.java
@@ -0,0 +1,146 @@
+package abstraction.eq4Transformateur1;
+
+import java.awt.Color;
+import java.util.List;
+
+import abstraction.eqXRomu.contratsCadres.Echeancier;
+import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eqXRomu.contratsCadres.IVendeurContratCadre;
+import abstraction.eqXRomu.contratsCadres.SuperviseurVentesContratCadre;
+import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.filiere.IActeur;
+import abstraction.eqXRomu.general.Journal;
+import abstraction.eqXRomu.general.Variable;
+import abstraction.eqXRomu.produits.IProduit;
+
+public class Transformateur1VendeurCCadre implements IVendeurContratCadre {
+
+	private static int PRIX_DEFAUT = 4500;
+	
+	private SuperviseurVentesContratCadre supCC;
+	private List<ExemplaireContratCadre> contratsEnCours;
+	private List<ExemplaireContratCadre> contratsTermines;
+	protected Journal journalCC;
+	
+	@Override
+	public void initialiser() {
+		super.initialiser();
+		this.supCC = (SuperviseurVentesContratCadre)(Filiere.LA_FILIERE.getActeur("Sup.CCadre"));
+		
+	}
+
+	@Override
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void next() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Variable> getIndicateurs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Variable> getParametres() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Journal> getJournaux() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setCryptogramme(Integer crypto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notificationFaillite(IActeur acteur) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notificationOperationBancaire(double montant) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<String> getNomsFilieresProposees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Filiere getFiliere(String nom) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getQuantiteEnStock(IProduit p, int cryptogramme) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean vend(IProduit produit) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Echeancier contrePropositionDuVendeur(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double propositionPrix(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double contrePropositionPrixVendeur(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void notificationNouveauContratCadre(ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double livrer(IProduit produit, double quantite, ExemplaireContratCadre contrat) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
