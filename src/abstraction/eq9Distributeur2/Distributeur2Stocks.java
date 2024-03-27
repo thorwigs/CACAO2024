dd2efa6e92a42f49271fbc7b94cf5c21d1e7cb52
margo <margo@172.16.136.212>
2024-03-27 11:50:42 +0100

    vente et stocks

--- src/abstraction/eq9Distributeur2/Distributeur2Stocks.java
+++ src/abstraction/eq9Distributeur2/Distributeur2Stocks.java
@@ -1,5 +1,18 @@
 package abstraction.eq9Distributeur2;
 
-public class Distributeur2Stocks {
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import abstraction.eqXRomu.general.Variable;
+import abstraction.eqXRomu.general.VariablePrivee;
+import abstraction.eqXRomu.produits.ChocolatDeMarque;
+
+
+public class Distributeur2Stocks extends Distributeur2Acteur{
+	protected HashMap<ChocolatDeMarque, Double> stockChocoMarque;
+	protected List<ChocolatDeMarque> chocolatsVillors;
+	protected Variable totalStocksChocoMarque;  
+
 
 }
