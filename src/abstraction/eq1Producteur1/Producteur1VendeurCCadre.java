1d58d916a8936c76fd5fd2c4c90268ba9619133a
user <user@172.16.129.14>
2024-03-27 11:29:35 +0100

    ouai ouai

--- src/abstraction/eq1Producteur1/Producteur1VendeurCCadre.java
+++ src/abstraction/eq1Producteur1/Producteur1VendeurCCadre.java
@@ -1,11 +1,32 @@
 package abstraction.eq1Producteur1;
+import java.util.LinkedList;
+import java.util.List;
+
 // Ce code a été fait par Haythem :)
 import abstraction.eqXRomu.contratsCadres.Echeancier;
 import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
 import abstraction.eqXRomu.contratsCadres.IVendeurContratCadre;
+import abstraction.eqXRomu.contratsCadres.SuperviseurVentesContratCadre;
+import abstraction.eqXRomu.general.Journal;
 import abstraction.eqXRomu.produits.IProduit;
 
 public class Producteur1VendeurCCadre extends Producteur1VendeurBourse implements IVendeurContratCadre {
+private static int PRIX_DEFAUT = 4500;
+	
+	private SuperviseurVentesContratCadre supCC;
+	private List<ExemplaireContratCadre> contratsEnCours;
+	private List<ExemplaireContratCadre> contratsTermines;
+	protected Journal journalCC;
+
+	public Producteur1VendeurCCadre() {
+		super();
+		this.contratsEnCours=new LinkedList<ExemplaireContratCadre>();
+		this.contratsTermines=new LinkedList<ExemplaireContratCadre>();
+		this.journalCC = new Journal(this.getNom()+" journal CC", this);
+	}
+
+
+	
 
 	@Override
 	public boolean vend(IProduit produit) {
