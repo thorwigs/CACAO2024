49bf34d1e59decd0f1b55b05f681830213714886
wiam <wiam@dhcp-132-148.mde.emn.fr>
2024-04-03 11:02:35 +0200

    vendeur 2

--- src/abstraction/eq8Distributeur1/Distributeur1Vendeur.java
+++ src/abstraction/eq8Distributeur1/Distributeur1Vendeur.java
@@ -1,17 +1,22 @@
 package abstraction.eq8Distributeur1;
 
+import java.util.List;
+
 import abstraction.eqXRomu.clients.ClientFinal;
 import abstraction.eqXRomu.filiere.IDistributeurChocolatDeMarque;
+import abstraction.eqXRomu.filiere.IMarqueChocolat;
 import abstraction.eqXRomu.produits.ChocolatDeMarque;
+import abstraction.eqXRomu.produits.Gamme;
 
-public class Distributeur1Vendeur extends Distributeur1Acteur implements IDistributeurChocolatDeMarque {
+public class Distributeur1Vendeur extends Distributeur1Acteur implements IDistributeurChocolatDeMarque, IMarqueChocolat {
 	private double capaciteDeVente;
 	private double[] prix;
 	private String[] marques;
 	
-	public Distributeur1Vendeur(ChocolatDeMarque[] chocos, double[] stocks, double capaciteDeVente, double[] prix, String[]marques) {
-		
-		
+	public Distributeur1Vendeur(double capaciteDeVente, double[] prix, String[]marques) {
+		this.capaciteDeVente = capaciteDeVente;
+		this.prix = prix;
+		this.marques = marques;
 	}
 
 	public double prix(ChocolatDeMarque choco) {
@@ -23,14 +28,38 @@ public class Distributeur1Vendeur extends Distributeur1Acteur implements IDistri
 		}
 	}
 
-
 	public double quantiteEnVente(ChocolatDeMarque choco, int crypto) {
-		return 0;
+		if (crypto!=this.cryptogramme) {
+			journal.ajouter("Quelqu'un essaye de me pirater !");
+			return 0.0;
+		} else {
+			int pos= (chocolats.indexOf(choco));
+			if (pos<0) {
+				return 0.0;
+			} else {
+				return Math.min(capaciteDeVente, this.getQuantiteEnStock(choco,crypto));
+			}
+		}
 	}
 
 
 	public double quantiteEnVenteTG(ChocolatDeMarque choco, int crypto) {
+		double capaciteTG = 0.1 * this.capaciteDeVente;
+		if (choco.getMarque()== "Chocoflow") {
+			return 0.06 * capaciteTG;
+		}
+		else {
+			if(choco.getChocolat().isEquitable()) {
+				if(choco.getChocolat().getGamme()==Gamme.MQ) {
+					return 0.01 * capaciteTG;
+				}
+				if(choco.getChocolat().getGamme()==Gamme.HQ) {
+					return 0.01 * capaciteTG;
+				}
+			}
+		}
 		return 0;
+
 	}
 
 
@@ -40,7 +69,13 @@ public class Distributeur1Vendeur extends Distributeur1Acteur implements IDistri
 
 
 	public void notificationRayonVide(ChocolatDeMarque choco, int crypto) {
-		
+		journal.ajouter(" Aie... j'aurais du mettre davantage de "+choco.getNom()+" en vente");
+	}
+
+	@Override
+	public List<String> getMarquesChocolat() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 
