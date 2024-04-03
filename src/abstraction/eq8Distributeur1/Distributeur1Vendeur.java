48a90d42f424a986bbac15d244086ef808f8f817
wiam <wiam@dhcp-132-148.mde.emn.fr>
2024-04-03 09:42:30 +0200

    Merge branch 'main' of https://github.com/CACAO2024/CACAO2024.git into main

--- src/abstraction/eq8Distributeur1/Distributeur1Vendeur.java
+++ src/abstraction/eq8Distributeur1/Distributeur1Vendeur.java
@@ -1,5 +1,38 @@
 package abstraction.eq8Distributeur1;
 
-public class Distributeur1Vendeur {
+import abstraction.eqXRomu.clients.ClientFinal;
+import abstraction.eqXRomu.filiere.IDistributeurChocolatDeMarque;
+import abstraction.eqXRomu.produits.ChocolatDeMarque;
+
+public class Distributeur1Vendeur extends Distributeur1Acteur implements IDistributeurChocolatDeMarque {
+	private double capaciteDeVente;
+	private double[] prix;
+	private String[] marques;
+
+		
+	public double prix(ChocolatDeMarque choco) {
+		return 0;
+	}
+
+
+	public double quantiteEnVente(ChocolatDeMarque choco, int crypto) {
+		return 0;
+	}
+
+
+	public double quantiteEnVenteTG(ChocolatDeMarque choco, int crypto) {
+		return 0;
+	}
+
+
+	public void vendre(ClientFinal client, ChocolatDeMarque choco, double quantite, double montant, int crypto) {
+		
+	}
+
+
+	public void notificationRayonVide(ChocolatDeMarque choco, int crypto) {
+		
+	}
+	
 
 }
