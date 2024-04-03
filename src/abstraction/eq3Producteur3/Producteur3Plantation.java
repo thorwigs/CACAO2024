3202fcedaeb45e631fdc5da63967b11aece33b02
Alexis <Alexis@DESKTOP-3CH9F3K>
2024-04-03 09:20:08 +0200

    Création fonction plantation() qui renvoie le nombre d'hectares de plantations associés à chaque type de fèves

--- src/abstraction/eq3Producteur3/Producteur3Plantation.java
+++ src/abstraction/eq3Producteur3/Producteur3Plantation.java
@@ -1,5 +1,29 @@
 package abstraction.eq3Producteur3;
 
+import java.util.HashMap;
+
+import abstraction.eqXRomu.produits.Feve;
+import abstraction.eqXRomu.produits.IProduit;
+
 public class Producteur3Plantation extends Producteur3Acteur {
 
+/**
+ * les variables surfaceXQ donnent le nombre d'hectares qui produisent des feves
+ * de qualité XQ
+ */
+	private double surfaceHQ;
+	private double surfaceMQ;
+	private double surfaceBQ;
+
+	/**
+	 * Le dictionnaire plantation a pour cle Feve 
+	 * et pour valeur la variable surfaceXQ associee du step precedent.
+	 * 
+	 */
+	protected HashMap<Feve, Double> plantation() {
+		HashMap<Feve, Double> h = new HashMap<Feve, Double>();
+		return h;
+	}
 }
+
+
