0c7af8d370d1d1cc4b34cad6c708785e721662cf
maxim <maxim@172.16.135.243>
2024-04-03 10:17:33 +0200

    bourse

--- src/abstraction/eq2Producteur2/Producteur2VendeurBourse.java
+++ src/abstraction/eq2Producteur2/Producteur2VendeurBourse.java
@@ -18,7 +18,7 @@ public class Producteur2VendeurBourse extends Producteur2_Plantation implements 
 
 	@Override
 	public double offre(Feve f, double cours) {
-		/*if (f.getGamme()==Gamme.MQ) {
+		if (f.getGamme()==Gamme.MQ) {
 			double offre = 10.0;
 			journalBourse.ajouter(Filiere.LA_FILIERE.getEtape()+" : je met en vente "+offre+" T de "+f);
 			return offre;
@@ -29,22 +29,21 @@ public class Producteur2VendeurBourse extends Producteur2_Plantation implements 
 		} else { // normalement impossible vu que le HQ n'est pas en bourse
 			journalBourse.ajouter(Filiere.LA_FILIERE.getEtape()+" : je met en vente 0.0 T de "+f);
 			return 0.0;
-		}*/
-		return 0.0;
+		}
 	}
 
 	@Override
 	public double notificationVente(Feve f, double quantiteEnT, double coursEnEuroParT) {
 		double retire = 10.0;
 
-		//this.stock.get(f).retirer(this, retire, cryptogramme);
-		//journalBourse.ajouter(Filiere.LA_FILIERE.getEtape()+" : j'ai vendu "+quantiteEnT+" T de "+f+" -> je retire "+retire+" T du stock qui passe a ");//+this.stock.get(f).getValeur((Integer)cryptogramme));
+		stock.put(f, stock.get(f)-retire);
+		journalBourse.ajouter(Filiere.LA_FILIERE.getEtape()+" : j'ai vendu "+quantiteEnT+" T de "+f+" -> je retire "+retire+" T du stock qui passe a "+this.stock.get(f));
 		return retire;
 	}
 
 	@Override
 	public void notificationBlackList(int dureeEnStep) {
-		//journalBourse.ajouter(Filiere.LA_FILIERE.getEtape()+" : je suis blackliste pour une duree de "+dureeEnStep+" etapes");
+		journalBourse.ajouter(Filiere.LA_FILIERE.getEtape()+" : je suis blackliste pour une duree de "+dureeEnStep+" etapes");
 	}
 
 }
