41a496f57fc31185e772854549c69a32b5271c46
Oscar <Oscar@172.16.131.99>
2024-04-03 09:13:22 +0200

    Merge branch 'main' of https://github.com/CACAO2024/CACAO2024.git into main

--- src/abstraction/eq4Transformateur1/Transformateur1.java
+++ src/abstraction/eq4Transformateur1/Transformateur1.java
@@ -1,7 +1,7 @@
 package abstraction.eq4Transformateur1;
 
 import abstraction.eqXRomu.filiere.Filiere;
-
+//
 public class Transformateur1 extends Transformateur1Vendeur  {
 	
 	public Transformateur1() {
