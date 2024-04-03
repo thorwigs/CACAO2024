fcbfa02f85adccb3f9d8e068d56e54117a78d9fd
Yuri69420 <146597745+Yuri69420@users.noreply.github.com>
2024-04-03 08:51:24 +0200

    Redoing the same damn thing

--- src/abstraction/eq3Producteur3/Producteur3.java
+++ src/abstraction/eq3Producteur3/Producteur3.java
@@ -1,8 +1,11 @@
 package abstraction.eq3Producteur3;
 
-public class Producteur3 extends Producteur3Acteur  {
+public class Producteur3 extends Producteur3VendeurContratCadre  {
 	
 	public Producteur3() {
 		super();
+	}
+	public void next() {
+		super.next();
 	}
 }
