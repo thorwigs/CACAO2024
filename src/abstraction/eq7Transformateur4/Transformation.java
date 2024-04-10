f41584ba48c25cbcdf521dea51dc7aba7fc58e67
Eliott <Eliott@172.16.131.229>
2024-04-10 12:25:22 +0200

    a

--- src/abstraction/eq7Transformateur4/Transformation.java
+++ src/abstraction/eq7Transformateur4/Transformation.java
@@ -22,10 +22,11 @@ public class Transformation extends Transformateur4VendeurAuxEncheres{
 		double critere = 200.0;
 		double peutproduireemploye = this.tauxproductionemploye*this.nbemployeCDI; //pour l'instant ça c'est 375, mais ça pourra évoluer si on change le nb d'employé
 		//il faudras s'adapter, pour utiliser qu'une partie de la main d'oeuvre pour faire tel ou tel chocolat, pour l'instant on fait qu'un seul chocolat
+		double stock_hg_be = this.stockFeves.get(Feve.F_HQ_BE);
+		double qtutile1 = 0 ; //correspond à la qte de fève qu'on va effectivement transformer
 		for (ChocolatDeMarque c : chocolatCocOasis) {
 			if (this.stockChocoMarque.get(c) < critere) {
-				double stock_hg_be = this.stockFeves.get(Feve.F_HQ_BE);
-				double qtutile1 ; //correspond à la qte de fève qu'on va effectivement transformer
+				
 				if (stock_hg_be > 0) {
 					if (stock_hg_be > peutproduireemploye) {
 					//on a assez en stock, on produit un maximum
@@ -41,20 +42,17 @@ public class Transformation extends Transformateur4VendeurAuxEncheres{
 				double payermachine = qtutile1*this.coutmachine; //prix des machines car on transforme une certaine qté de fèves
 				double pourcentageadjuvant = this.pourcentageTransfo.get(Feve.F_HQ_BE).get(Chocolat.C_HQ_BE)-1;
 				double payeradjuvant = this.coutadjuvant*pourcentageadjuvant*qtutile1;
+				
+				
+				totalStocksChocoMarque.ajouter(this, qtutile1, cryptogramme);
+				
 			}//else : on a assez de ce choco de marque, on en produit pas
 			//là meme chose pour tout les chocos
 		}
 		//refaire des boucles for pour les chocos qui n'ont pas de marques
 			
 			
-			this.journalTransfo.ajouter("Stock avant trnasfo " + this.totalStocksChocoMarque.getValeur(this.cryptogramme) );
-			totalStocksChocoMarque.ajouter(this, qtutile1, cryptogramme);
-			this.journalTransfo.ajouter("Stock après trnasfo " + this.totalStocksChocoMarque.getValeur(this.cryptogramme) );
-			
-			
-			
-			this.journalTransfo.ajouter("On à transformé " + qtutile1 + "T de Choco");
-		
+
 		//là on paye les trucs 
 	}
 	
