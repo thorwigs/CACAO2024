9a0aae89e9bd68f467f0fae7892d01ff971c6ad1
pierr <pierr@172.16.136.142>
2024-04-10 12:18:34 +0200

    modif transfo

--- src/abstraction/eq7Transformateur4/Transformation.java
+++ src/abstraction/eq7Transformateur4/Transformation.java
@@ -23,9 +23,9 @@ public class Transformation extends Transformateur4VendeurAuxEncheres{
 		double peutproduireemploye = this.tauxproductionemploye*this.nbemployeCDI; //pour l'instant ça c'est 375, mais ça pourra évoluer si on change le nb d'employé
 		//il faudras s'adapter, pour utiliser qu'une partie de la main d'oeuvre pour faire tel ou tel chocolat, pour l'instant on fait qu'un seul chocolat
 		for (ChocolatDeMarque c : chocolatCocOasis) {
+			double qtutile1 = 0; //correspond à la qte de fève qu'on va effectivement transformer
+			double stock_hg_be = this.stockFeves.get(Feve.F_HQ_BE);
 			if (this.stockChocoMarque.get(c) < critere) {
-				double stock_hg_be = this.stockFeves.get(Feve.F_HQ_BE);
-				double qtutile1 ; //correspond à la qte de fève qu'on va effectivement transformer
 				if (stock_hg_be > 0) {
 					if (stock_hg_be > peutproduireemploye) {
 					//on a assez en stock, on produit un maximum
@@ -37,7 +37,8 @@ public class Transformation extends Transformateur4VendeurAuxEncheres{
 				} //else : on fait rien car on peut pas produire
 				this.stockFeves.replace(Feve.F_HQ_BE, stock_hg_be - qtutile1);//on retire qtutile1 du stock de feve haut de gamme pour faire du chocolat
 				double qtechocoproduit = qtutile1*this.pourcentageTransfo.get(Feve.F_HQ_BE).get(Chocolat.C_HQ_BE); //la qte de choco produit à partir de qtutile1
-				
+				this.stockChocoMarque.replace(c, this.stockChocoMarque.get(c) + qtechocoproduit);
+				this.totalStocksChocoMarque.ajouter(this, qtechocoproduit, cryptogramme);
 				double payermachine = qtutile1*this.coutmachine; //prix des machines car on transforme une certaine qté de fèves
 				double pourcentageadjuvant = this.pourcentageTransfo.get(Feve.F_HQ_BE).get(Chocolat.C_HQ_BE)-1;
 				double payeradjuvant = this.coutadjuvant*pourcentageadjuvant*qtutile1;
@@ -46,13 +47,7 @@ public class Transformation extends Transformateur4VendeurAuxEncheres{
 		}
 		//refaire des boucles for pour les chocos qui n'ont pas de marques
 			
-			
-			this.journalTransfo.ajouter("Stock avant trnasfo " + this.totalStocksChocoMarque.getValeur(this.cryptogramme) );
-			totalStocksChocoMarque.ajouter(this, qtutile1, cryptogramme);
-			this.journalTransfo.ajouter("Stock après trnasfo " + this.totalStocksChocoMarque.getValeur(this.cryptogramme) );
-			
-			this.journalTransfo.ajouter("On à transformé " + qtutile1 + "T de Choco");
-		
+
 		//là on paye les trucs 
 	}
 	
