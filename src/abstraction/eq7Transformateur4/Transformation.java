e3d6e922a375962fedcd3a0bb2bdb4326ecf02c1
pierr <pierr@172.16.136.142>
2024-04-10 09:18:32 +0200

    Merge branch 'main' of https://github.com/CACAO2024/CACAO2024.git into main

--- src/abstraction/eq7Transformateur4/Transformation.java
+++ src/abstraction/eq7Transformateur4/Transformation.java
@@ -1,5 +1,8 @@
 package abstraction.eq7Transformateur4;
 
+import abstraction.eqXRomu.produits.Chocolat;
+import abstraction.eqXRomu.produits.Feve;
+
 public class Transformation extends Transformateur4VendeurAuxEncheres{
 	
 	//objectifs : vérifier le stocks de fève : s'il est suffisant pour une fève, produire du chocolat avec, puis attribuer aux chocolat une marque ou nom.
@@ -10,6 +13,22 @@ public class Transformation extends Transformateur4VendeurAuxEncheres{
 	
 	public void next() {
 		super.next();
+		double stock_hg_be = this.stockFeves.get(abstraction.eqXRomu.produits.Feve.F_HQ_BE);
+		if (stock_hg_be > 0) {
+			if (stock_hg_be > 500) {
+				this.stockFeves.replace(abstraction.eqXRomu.produits.Feve.F_HQ_BE, stock_hg_be - 500);
+				//on retire 500 du stock de haut de gamme pour faire du chocolat
+				double qtechocoproduit = 500*this.pourcentageTransfo.get(Feve.F_HQ_BE).get(Chocolat.C_HQ_BE);
+				
+				
+			} else {
+				this.stockFeves.replace(abstraction.eqXRomu.produits.Feve.F_HQ_BE, 0.0);
+				//on retire tout notre stock de haut de gamme pour faire du chocolat
+				
+				
+			}
+		}
+		
 		
 		//jsp si c'est là qu'il faut l'ajouter...
 		
