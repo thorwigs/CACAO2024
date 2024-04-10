1bfa25b0784db2c14bee4beec75dabf925d20c83
galem <galem@172.16.137.117>
2024-04-10 11:39:30 +0200

    début init

--- src/abstraction/eq3Producteur3/Producteur3Acteur.java
+++ src/abstraction/eq3Producteur3/Producteur3Acteur.java
@@ -68,13 +68,22 @@ public abstract class Producteur3Acteur implements IActeur {
 		setQuantiteEnStock(Feve.F_HQ_BE,100000);
 		//
 		//On set les productions
+		/**
+		 *Initialisation basé sur les quantités produites actuellement au Pérou
+		 *surface:
+		HQ_BE : 202 ha
+		HQ : 727,785 ha   (Non Bio équitable) 
+		MQ : 1 427 ha
+		BQ : 3 234.6 ha
+		 * @author galem (Gabin)
+		 */
 		HashMap<Feve,Double> d01 = new HashMap<Feve,Double>();
-		d01.put(Feve.F_BQ, 20000.0);
-		d01.put(Feve.F_MQ, 20000.0);
-		d01.put(Feve.F_MQ_E, 20000.0);
-		d01.put(Feve.F_HQ, 20000.0);
-		d01.put(Feve.F_HQ_E, 20000.0);
-		d01.put(Feve.F_HQ_BE, 20000.0);
+		d01.put(Feve.F_BQ, 181.95);
+		d01.put(Feve.F_MQ, 60.6475);		//80% de HQ est non équitable
+		d01.put(Feve.F_MQ_E, 15.16);        //20% de MQ est équitable
+		d01.put(Feve.F_HQ, 41.70);			//60% de HQ est ni bio ni équitable
+		d01.put(Feve.F_HQ_E, 18.95);		//20% de HQ est équitable
+		d01.put(Feve.F_HQ_BE, 9.09);		//20% de HQ est bio équitable
 		setProdTemps(d01,d01);
 		//On set les variables coutGammeStep
 		this.coutGammeStep = new HashMap<Feve,HashMap<Integer,Double>>();
