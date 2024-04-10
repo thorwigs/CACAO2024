e400b6750b0d611d8b9d51c92209140f629804f3
Erwann <erwannbarrelebiavant@dhcp-207-33.eduroam.emn.fr>
2024-04-10 09:06:55 +0200

    Merge branch 'main' of https://github.com/R0000BIN/CACAO2024 into main

--- src/abstraction/eq5Transformateur2/Transformateur2.java
+++ src/abstraction/eq5Transformateur2/Transformateur2.java
@@ -1,6 +1,6 @@
 package abstraction.eq5Transformateur2;
 
-public class Transformateur2 extends Transformateur2VendeurAuxEncheres  {
+public class Transformateur2 extends Transformateur2VendeurAppelDOffre  {
 	
 	public Transformateur2() {
 		super();
