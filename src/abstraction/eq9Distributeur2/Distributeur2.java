0a26255d24281c126e2f76c4a828f9dced87893d
margo <margo@172.16.136.212>
2024-04-10 10:48:53 +0200

    Merge branch 'main' of https://github.com/CACAO2024/CACAO2024.git into main

--- src/abstraction/eq9Distributeur2/Distributeur2.java
+++ src/abstraction/eq9Distributeur2/Distributeur2.java
@@ -1,6 +1,6 @@
 package abstraction.eq9Distributeur2;
 
-public class Distributeur2 extends Distributeur2AppelOffre  {
+public class Distributeur2 extends Distributeur2MarqueDistributeur  {
 	
 	public Distributeur2() {
 		super();
