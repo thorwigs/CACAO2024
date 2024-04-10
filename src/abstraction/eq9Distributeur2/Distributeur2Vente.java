c0a2d8b833ef49a1ebc978d44d390d86e4374dd3
margo <margo@172.16.136.212>
2024-04-10 08:12:16 +0200

    Merge branch 'main' of https://github.com/Maxime-cod/CACAO2024 into main

--- src/abstraction/eq9Distributeur2/Distributeur2Vente.java
+++ src/abstraction/eq9Distributeur2/Distributeur2Vente.java
@@ -16,7 +16,8 @@ public class Distributeur2Vente extends Distributeur2Stocks implements IDistribu
 	@Override
 	public double quantiteEnVente(ChocolatDeMarque choco, int crypto) {
 		// TODO Auto-generated method stub
-		return 10000;//this.getQuantiteEnStock(choco, crypto);
+
+		return this.getStockChocoMarque(choco,crypto);
 	}
 
 	@Override
@@ -39,6 +40,9 @@ public class Distributeur2Vente extends Distributeur2Stocks implements IDistribu
 	@Override
 	public void notificationRayonVide(ChocolatDeMarque choco, int crypto) {
 		// TODO Auto-generated method stub
+	if (this.getStockChocoMarque(choco, crypto)==0.0) {
+		journal.ajouter("plus de chocolat"+choco+"");
+	}
 		
 	}
 
