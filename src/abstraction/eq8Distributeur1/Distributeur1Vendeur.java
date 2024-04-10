8c05576a86391c5ea969676ad7c2efa47b77ce6e
wiam <wiam@dhcp-132-148.mde.emn.fr>
2024-04-10 10:14:14 +0200

    Merge branch 'main' of https://github.com/ghwiwi/CACAO2024 into main

--- src/abstraction/eq8Distributeur1/Distributeur1Vendeur.java
+++ src/abstraction/eq8Distributeur1/Distributeur1Vendeur.java
@@ -2,6 +2,7 @@ package abstraction.eq8Distributeur1;
 
 
 import abstraction.eqXRomu.clients.ClientFinal;
+import abstraction.eqXRomu.filiere.Filiere;
 import abstraction.eqXRomu.filiere.IDistributeurChocolatDeMarque;
 import abstraction.eqXRomu.produits.ChocolatDeMarque;
 import abstraction.eqXRomu.produits.Gamme;
@@ -68,6 +69,16 @@ public class Distributeur1Vendeur extends Distributeur1Acteur implements IDistri
 
 	public void notificationRayonVide(ChocolatDeMarque choco, int crypto) {
 		journal.ajouter(" Aie... j'aurais du mettre davantage de "+choco.getNom()+" en vente");
+	}
+	public void next() {
+		super.next();
+		journal.ajouter("Etape="+Filiere.LA_FILIERE.getEtape());
+		if (Filiere.LA_FILIERE.getEtape()>=1) {
+			for (int i=0; i<this.chocolats.size(); i++) {
+			journal.ajouter("Le prix moyen du chocolat \""+chocolats.get(i).getNom()+"\" a l'etape precedente etait de "+Filiere.LA_FILIERE.prixMoyen(chocolats.get(i), Filiere.LA_FILIERE.getEtape()-1));
+			journal.ajouter("Les ventes de chocolat \""+chocolats.get(i)+" il y a un an etaient de "+Filiere.LA_FILIERE.getVentes(chocolats.get(i), Filiere.LA_FILIERE.getEtape()-24));
+			}
+		}
 	}
 
 }
