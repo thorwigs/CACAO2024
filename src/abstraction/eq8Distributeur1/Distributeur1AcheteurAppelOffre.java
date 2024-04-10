2229f1124bcc00fec9913233ad3db7bcda74ea73
cleme <cleme@172.16.132.197>
2024-04-10 11:55:24 +0200

    AO4# WARNING: head commit changed in the meantime

--- src/abstraction/eq8Distributeur1/Distributeur1AcheteurAppelOffre.java
+++ src/abstraction/eq8Distributeur1/Distributeur1AcheteurAppelOffre.java
@@ -36,8 +36,40 @@ public class Distributeur1AcheteurAppelOffre extends Distributeur1AcheteurContra
 	
 	
 	public OffreVente choisirOV(List<OffreVente> propositions) {
-		
-		return null;
+		double solde = Filiere.LA_FILIERE.getBanque().getSolde(this, cryptogramme);
+		int moins_cher_total=0;
+		for (int i=0; i<propositions.size();i++) {
+			if (propositions.get(moins_cher_total).getPrixT()*propositions.get(moins_cher_total).getQuantiteT()>propositions.get(i).getPrixT()*propositions.get(i).getQuantiteT()) {
+				moins_cher_total=i;
+			}
+		}
+		if ((solde<propositions.get(moins_cher_total).getPrixT()*propositions.get(moins_cher_total).getQuantiteT())
+				&& (solde<propositions.get(0).getPrixT()*propositions.get(0).getQuantiteT())) {
+			journalAO.ajouter("   refus de l'AO : pas assez d'argent sur le compte");
+			return null;
+		}
+		int choisi=-1; // permet de connaître la proposition choisi à la fin, la moins chere, ou renverra -1 si pas d'offre correspondante
+		int parcourir=0; //permet de parcourir la liste des propositions pour trouver la bonne
+		while(choisi==-1 && parcourir<propositions.size()) {
+			if (propositions.get(0).getOffre().getProduit().equals(propositions.get(parcourir).getProduit())!=true) {
+				parcourir++;
+			}
+			else {
+				choisi=parcourir;
+			}
+		}
+		if (choisi==-1) {
+			journalAO.ajouter("   refus de l'AO : produit pas correspondant à la demande");
+			return null;
+		}
+		if ((solde<propositions.get(choisi).getPrixT()*propositions.get(choisi).getQuantiteT()))
+				 {
+			journalAO.ajouter("   refus de l'AO : pas assez d'argent sur le compte");
+			return null;
+				 }
+		else {
+			return propositions.get(choisi);
+		}
 	}
 	
 	public List<Journal> getJournaux(){
