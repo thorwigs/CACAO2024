22f446b43fc1a11707fce9bf502f80f1de376056
youss <youss@172.16.137.163>
2024-04-11 20:51:43 +0200

    .

--- src/abstraction/eq1Producteur1/Ouvrier.java
+++ src/abstraction/eq1Producteur1/Ouvrier.java
@@ -12,12 +12,23 @@ public class Ouvrier extends Producteur1Acteur {
 	public double salaire;
 	public boolean isEquitable;
 	public boolean isForme;
-	public Ouvrier(double anciennete,double rendement,double salaire,boolean isForme) {
+	public boolean estEnfant;
+	public Ouvrier() {
+		this.rendement=1;
+		this.anciennete=0;
+		this.salaire=1.8;
+		this.isForme=false;
+		this.isEquitable=false;
+		this.estEnfant=false;	
+	}
+	public Ouvrier(double anciennete,double rendement,double salaire,boolean isForme,boolean isEquitable,boolean estEnfant) {
 		
 		this.rendement=rendement;
 		this.anciennete=anciennete;
 		this.salaire=salaire;
 		this.isForme = isForme;
+		this.isEquitable=isEquitable;
+		this.estEnfant=estEnfant;
 	}
 	public double getAnciennete() {
 		return anciennete;
@@ -51,6 +62,9 @@ public class Ouvrier extends Producteur1Acteur {
 	public void setIsForme(boolean bool) {
 		 this.isForme=bool;
 	}
+	public boolean getIsEnfant() {
+		return this.estEnfant;
+	}
 		
 		
 	
@@ -58,7 +72,11 @@ public class Ouvrier extends Producteur1Acteur {
 		double ancienneteMin =10;
 		double augmentationRendement=0.5;
 		double augmentationSalaire=0.1*this.getSalaire();
-		if ( this.getAnciennete()<= ancienneteMin) {
+		if (( this.getAnciennete()<= ancienneteMin) && 
+				(!(this.getIsForme())) &&
+				(!(this.getIsEnfant())))
+		
+			{
 			this.setRendement(this.getRendement()+ augmentationRendement);
 			this.setSalaire(this.getSalaire()+augmentationSalaire);
 			this.setIsForme(true);
@@ -83,6 +101,36 @@ public class Ouvrier extends Producteur1Acteur {
 		return s;
 		
 	}
+	public double getSalaireEquitable(ArrayList<Ouvrier> listeOuvrier, double salaireEquitable) {
+			
+			double s =0;
+			for (Ouvrier ouvrier : listeOuvrier) {
+				if (ouvrier.isEquitable) {
+					s=s+ouvrier.getSalaire();
+				}
+				
+				
+			}
+			return s;
+			
+		}
+	public double getSalaireEnfants(ArrayList<Ouvrier> listeOuvrier, double salaireEnfant) {
+			
+			double s =0;
+			for (Ouvrier ouvrier : listeOuvrier) {
+				if (ouvrier.getIsEnfant()) {
+					s=s+ouvrier.getSalaire();
+				}
+				
+				
+			}
+			return s;
+			
+		}
+
+
+
+
 	public int GetNombreOuvrierEquitable(ArrayList<Ouvrier> listeOuvrier) {
 		int s=0;
 		for (Ouvrier ouvrier : listeOuvrier) {
@@ -101,10 +149,50 @@ public class Ouvrier extends Producteur1Acteur {
 		return listeOuvrier.size()-s;
 		
 	}
-	public double getSalaireEquitable(ArrayList<Ouvrier> listeOuvrier) {
+	
+	public int getNombreOuvrierFormés(ArrayList<Ouvrier> listeOuvrier) {
 		
-		return getNbOuvEq()*3;
+		int s=0;
+		for (Ouvrier ouvrier : listeOuvrier) {
+			if (ouvrier.isForme) {
+				s=s+1;
+				
+			}
+			
+			
+		}
+		return s;
 	}
+	
+	public void addOuvrier(ArrayList<Ouvrier> listeOuvriers,int nombre_à_ajouter,double anciennete,double salaire,double rendement,boolean isEquitable,boolean isForme,boolean isEnfant) {
+		
+		for (int i = 0; i < nombre_à_ajouter; i++) {
+			Ouvrier ouvrier_a_ajouter=new Ouvrier(anciennete,rendement,salaire,isForme,isEquitable,isEnfant);
+			listeOuvriers.add(ouvrier_a_ajouter);
+			
+		}
+		
+	}
+	public boolean equals(Object objet) {
+		return (objet instanceof Ouvrier)&&
+				(((Ouvrier) objet).getIsEquitable()==this.getIsEquitable())&&
+				((((Ouvrier) objet).getAnciennete()==this.getAnciennete()))&&
+				((((Ouvrier) objet).getIsForme()==this.getIsForme()))&&
+				((((Ouvrier) objet).getSalaire()==this.getSalaire()))&&
+				((((Ouvrier) objet).getRendement()==this.getRendement()));
+				
+	}
+	public String toString() {
+	    return "Ouvrier{ " +
+	            " anciennete= " + anciennete +
+	            " , rendement= " + rendement +
+	            " , salaire= " + salaire +
+	            " , isEquitable= " + isEquitable +
+	            " , isForme= " + isForme +
+	            '}';
+	}
+	
+	
 	
 	
 	
