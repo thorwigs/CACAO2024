7c16b8f341cc85db8f57cfed4a90632f933f1bf9
Erwann <erwannbarrelebiavant@dhcp-207-33.eduroam.emn.fr>
2024-04-17 08:54:53 +0200

    paiements mis à jour

--- src/abstraction/eq5Transformateur2/Transformateur2MasseSalariale.java
+++ src/abstraction/eq5Transformateur2/Transformateur2MasseSalariale.java
@@ -2,6 +2,7 @@ package abstraction.eq5Transformateur2;
 
 import abstraction.eqXRomu.filiere.Banque;
 import abstraction.eqXRomu.filiere.Filiere;
+import abstraction.eqXRomu.general.Journal;
 import abstraction.eqXRomu.produits.Chocolat;
 import abstraction.eqXRomu.produits.ChocolatDeMarque;
 import abstraction.eqXRomu.produits.Feve;
@@ -18,6 +19,7 @@ public class Transformateur2MasseSalariale extends Transformateur2Acteur {
 	protected double salaire;//salaire par step
 	protected double coutLicenciement1Salarie;
 	protected double capaciteTransformation;// tonnes transformées par 1 salarié par step
+	protected Journal JournalMasseSalariale;
 	
 	
 	public Transformateur2MasseSalariale() {
@@ -29,21 +31,42 @@ public class Transformateur2MasseSalariale extends Transformateur2Acteur {
 		salaire = 2000;
 		coutLicenciement1Salarie = 4*salaire;
 		capaciteTransformation = 3.7;
+		this.JournalMasseSalariale.ajouter(""); // ajouter les infos 
+
 	}
 	
 	
 	////// A FINIR /////////////////////////////////////////
-	public double TonnesTransformees(Feve f, Chocolat c) {
+	public double TonnesTransformees(Feve f) {
 		// modif des stocks
 		return 2;
 	}
 	///////////////////////////////////////////////////////
+	
+	public double TotauxTonnesTransformees() {
+		double totaux = 0;
+		for (Feve f : Feve.values()) {
+			totaux += TonnesTransformees(f);
+		}
+		this.JournalMasseSalariale.ajouter("On a Transformé au total "+totaux+" tonnes de chocolat");
+		return totaux;
+	}
 	
 	public double CoutTransformation(ChocolatDeMarque cm, double tonnes) {
 		// 8 = coût machines pour une tonne par step
 		// 1200 = coût adjuvants pour une tonne par step
 		return tonnes*8 + tonnes*(1-cm.getPourcentageCacao())*1200 ;
 	}
+	public double CoutTransformationTotal() {
+		double coutTotal = 0;
+		for (ChocolatDeMarque cm : Filiere.LA_FILIERE.getChocolatsProduits()) {
+			coutTotal += CoutTransformation(cm,t);	
+		}
+		this.JournalMasseSalariale.ajouter("Le cout total de la transformation est de"+coutTotal);
+		return coutTotal;
+	}
+	
+	
 	
 	public int EmbaucheLicenciement(double TonnesTransformees) {
 		double CapaciteTransfoTotale = NbSalaries * capaciteTransformation;
@@ -59,7 +82,6 @@ public class Transformateur2MasseSalariale extends Transformateur2Acteur {
 			return licenciement;
 		}
 	}
-	
 	public double CoutMasseSalariale(double TonnesTransformees) {
 		double cout_salaire = NbSalaries * salaire;
 		double cout_licenciement = 0;
@@ -70,21 +92,17 @@ public class Transformateur2MasseSalariale extends Transformateur2Acteur {
 	}
 	
 	
-	
-	////// A FINIR //////////////////////////////////////////
 	public void next() {
 		super.next();
 		
 		// Paiement des coût de la masse salariale
-		double TonnesTransformees = 2;
-		Filiere.LA_FILIERE.getBanque().payerCout(Filiere.LA_FILIERE.getActeur(getNom()), this.cryptogramme, "Coût Masse Salariale", CoutMasseSalariale(TonnesTransformees));
+		double TotauxTransformees = TotauxTonnesTransformees();
+		Filiere.LA_FILIERE.getBanque().payerCout(Filiere.LA_FILIERE.getActeur(getNom()), this.cryptogramme, "Coût Masse Salariale", CoutMasseSalariale(TotauxTransformees));
 		
-		// Paiement des coût de transformation pour chaque chocolat de marque
-		/*for (ChocolatDeMarque cm : ) {
-			double tonnes = 1;
-			Filiere.LA_FILIERE.getBanque().payerCout(Filiere.LA_FILIERE.getActeur(getNom()), this.cryptogramme, "Coût transformation de"+tonnes+"tonnes de"+cm.getMarque(), CoutMasseSalariale(TonnesTransformees));
-		}*/
-	}
-	////////////////////////////////////////////////////////
-
+		// Paiement des coût de transformation
+		double TotalCout = CoutTransformationTotal();
+		Filiere.LA_FILIERE.getBanque().payerCout(Filiere.LA_FILIERE.getActeur(getNom()), this.cryptogramme, "Coût Transformation" , TotalCout);
+		}
 }
+
+
