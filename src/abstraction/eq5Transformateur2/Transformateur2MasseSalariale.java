393669e7d932c36c2e77e102771a136e3ac02be5
vdant <vdant@172.16.132.62>
2024-04-17 11:54:09 +0200

    Merge branch 'main' of https://github.com/CACAO2024/CACAO2024 into main

--- src/abstraction/eq5Transformateur2/Transformateur2MasseSalariale.java
+++ src/abstraction/eq5Transformateur2/Transformateur2MasseSalariale.java
@@ -44,14 +44,14 @@ public class Transformateur2MasseSalariale extends Transformateur2Acteur {
 	}
 	
 
-	public double TonnesTransformees(Feve f) {
+	public double TonnesTransformees(Feve f) {//victor
 		double tMaxTransformees = Math.min(this.getQuantiteEnStock(f, cryptogramme),this.NbSalaries*this.capaciteTransformation); //Quantite maximale a transformer
 		double tonnesTransformees =0.9*tMaxTransformees; //On transforme 90% (peut etre modifie) de ce qu'on peut transformer au maximum
 		Chocolat c = Chocolat.get(f.getGamme(), f.isBio(), f.isEquitable());
 		this.stockChoco.put(c, this.getQuantiteEnStock(c,cryptogramme)+tonnesTransformees); //Modifie le stock de tablettes
 		this.stockFeves.put(f, this.getQuantiteEnStock(f,cryptogramme)-tonnesTransformees); //Modifie le stock de feves
 		return tonnesTransformees; 
-	}
+	} 
 	
 	public double TotauxTonnesTransformees() {
 		double totaux = 0;
