22e24a38a280005e49f0fa7c19052f95e99016c9
mdura <mdura@LAPTOP-1D0CE7TL>
2024-04-17 10:06:51 +0200

    on paye les couts de stockages!!!!(normalement)

--- src/abstraction/eq9Distributeur2/Distributeur2.java
+++ src/abstraction/eq9Distributeur2/Distributeur2.java
@@ -1,12 +1,17 @@
 package abstraction.eq9Distributeur2;
 
+import abstraction.eqXRomu.filiere.Filiere;
+
 public class Distributeur2 extends Distributeur2MarqueDistributeur  {
+	private double frais;
 	
 	public Distributeur2() {
 		super();
 	}
 	public void next() {
 		super.next();
-		System.out.println(this.totalStocksChocoMarque.getValeur(cryptogramme));
+		frais = this.totalStocksChocoMarque.getValeur(cryptogramme)*this.getCoutStockage();
+		if (frais>0) {
+		Filiere.LA_FILIERE.getBanque().payerCout(this, cryptogramme, getDescription(), frais);}
 		}
 }
