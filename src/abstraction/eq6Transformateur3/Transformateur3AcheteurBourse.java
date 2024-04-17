09a8b14bc934229d508f731862f810a12a3bf601
cchau <cchau@172.16.132.11>
2024-04-17 12:02:56 +0200

    Bourse

--- src/abstraction/eq6Transformateur3/Transformateur3AcheteurBourse.java
+++ src/abstraction/eq6Transformateur3/Transformateur3AcheteurBourse.java
@@ -1,16 +1,30 @@
 package abstraction.eq6Transformateur3;
 
+import java.util.LinkedList;
+
 import abstraction.eqXRomu.bourseCacao.IAcheteurBourse;
+import abstraction.eqXRomu.contratsCadres.ExemplaireContratCadre;
+import abstraction.eqXRomu.general.Journal;
 import abstraction.eqXRomu.produits.Feve;
 
 public class Transformateur3AcheteurBourse extends Transformateur3VendeurCCadre implements IAcheteurBourse {
+	
+	protected Journal journalBourse;
 
+	public Transformateur3AcheteurBourse() {
+		//super();
+		
+		this.journalBourse = new Journal(this.getNom()+" journal Bourse", this);
+	}
+	
 	@Override
 	public double demande(Feve f, double cours) {
 		if (this.stockFeves.get(f)<30000) {
-			return this.stockFeves.get(f); 
+			this.journalBourse.ajouter("Achat en bourse de " + (30000 - this.stockFeves.get(f)) + " fèves "+ f);
+			return 30000-this.stockFeves.get(f); 
+			
 		}
-		
+		this.journalBourse.ajouter(" ");
 		return 0;
 	}
 
@@ -18,6 +32,7 @@ public class Transformateur3AcheteurBourse extends Transformateur3VendeurCCadre 
 	public void notificationAchat(Feve f, double quantiteEnT, double coursEnEuroParT) {
 		this.stockFeves.put(f, this.stockFeves.get(f)+quantiteEnT);
 		this.totalStocksFeves.ajouter(this, quantiteEnT, cryptogramme);
+		
 	}
 
 	@Override
