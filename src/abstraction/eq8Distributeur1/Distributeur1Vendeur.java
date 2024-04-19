72eca6fe1f127775f032c5bbb1e5d7e55af5e600
ianis <ianis@Ianis>
2024-04-19 18:19:28 +0200

    fcyugihuo

--- src/abstraction/eq8Distributeur1/Distributeur1Vendeur.java
+++ src/abstraction/eq8Distributeur1/Distributeur1Vendeur.java
@@ -241,7 +241,6 @@ public class Distributeur1Vendeur extends Distributeur1Acteur implements IDistri
 		journalVente.ajouter("");
 		this.setNombreEmploye();
 		Filiere.LA_FILIERE.getBanque().payerCout(Filiere.LA_FILIERE.getActeur(getNom()), cryptogramme, "Coût Fixe", this.Cout_Fixe());
-		
 	}
 	
 
