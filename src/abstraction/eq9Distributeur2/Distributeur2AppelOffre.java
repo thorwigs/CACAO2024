2137f6c0ea02037be6b3ea973f5a5000e5da4d1f
mdura <mdura@LAPTOP-1D0CE7TL>
2024-04-19 11:42:34 +0200

    appel d'offre moins de 5 tonnes

--- src/abstraction/eq9Distributeur2/Distributeur2AppelOffre.java
+++ src/abstraction/eq9Distributeur2/Distributeur2AppelOffre.java
@@ -42,7 +42,7 @@ public abstract class Distributeur2AppelOffre extends Distributeur2ContratCadre 
 
 	public void FaireAppelDOffre() {
 		for (ChocolatDeMarque chocolat : this.stockChocoMarque.keySet()) {
-			if (this.stockChocoMarque.get(chocolat)<=10) {
+			if (this.stockChocoMarque.get(chocolat)<=5) {
 				
 				Double quantite = Filiere.LA_FILIERE.getVentes(chocolat, -24);
 				OffreVente propRetenue=((SuperviseurVentesAO) Filiere.LA_FILIERE.getActeur("Sup.AO")).acheterParAO(this,this.cryptogramme,chocolat,quantite);
