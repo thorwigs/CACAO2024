a2f3a199ead79316149c15d476eb7b03379004c5
Eliott <Eliott@172.16.138.32>
2024-04-19 16:08:31 +0200

    nom des auteurs

--- src/abstraction/eq7Transformateur4/Transformateur4VendeurAppelDOffre.java
+++ src/abstraction/eq7Transformateur4/Transformateur4VendeurAppelDOffre.java
@@ -17,6 +17,9 @@ import abstraction.eqXRomu.produits.Feve;
 import abstraction.eqXRomu.produits.Gamme;
 import abstraction.eqXRomu.produits.IProduit;
 
+//classe 'codé' par Eliott, néanmoins ce n'est pour l'instant qu'un copé-collé de l'exemple de Vendeur Appel d'offre fourni
+
+
 public class Transformateur4VendeurAppelDOffre extends Transformation implements IVendeurAO {
 	private HashMap<ChocolatDeMarque, List<Double>> prixAO;
 	protected Journal journalAO;
