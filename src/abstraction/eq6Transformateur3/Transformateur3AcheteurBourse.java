4e5b811cd32f6d3d9b3a9fe2d4cccda20a2f9e61
Arthur <Arthur@172.16.132.167>
2024-04-19 14:22:47 +0200

    livrable

--- src/abstraction/eq6Transformateur3/Transformateur3AcheteurBourse.java
+++ src/abstraction/eq6Transformateur3/Transformateur3AcheteurBourse.java
@@ -77,7 +77,7 @@ public class Transformateur3AcheteurBourse extends Transformateur3VendeurCCadre 
 	
 
 	/**
-	 * @author Cédric 
+	 * @author Arthur 
 	 * 
 	 */	
 
@@ -93,7 +93,7 @@ public class Transformateur3AcheteurBourse extends Transformateur3VendeurCCadre 
 	
 	}
 	/**
-	 * @author Cédric 
+	 * @author Arthur 
 	 * 
 	 */
 	public List<Journal> getJournaux() {
