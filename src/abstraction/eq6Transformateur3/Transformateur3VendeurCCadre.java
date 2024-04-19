120081300aad2ac308e970ee56b9cdb6f34da047
cchau <cchau@CE-DELL-IC.home>
2024-04-19 20:22:16 +0200

    Correction vendeur CC

--- src/abstraction/eq6Transformateur3/Transformateur3VendeurCCadre.java
+++ src/abstraction/eq6Transformateur3/Transformateur3VendeurCCadre.java
@@ -125,7 +125,7 @@ public class Transformateur3VendeurCCadre extends Transformateur3AcheteurCCadre 
 		double moyenne = somme/p;
 		prix = moyenne + 0.5 * 1200 + 8; // prise en compte du cout de production ( pas exactement car non prise en compte de la qualité de notre chocolat,0.5 choisi arbitrairementet(pourcentage d'adjuvants)) et du prix moyen de la tonne de fève qu'on achète dans nos contrats en cours
 		
-		return 1.03 * prix; // un petit +3% pour maximiser le prfofit
+		return 1.03 * prix; //  un petit +3% pour maximiser le prfofit
 		
 	}
 	/**
