578c8ed8cc32dc7b60bc9ebddf614463cc41a22e
mdura <mdura@LAPTOP-1D0CE7TL>
2024-04-19 12:20:14 +0200

    stocks bizarres

--- src/abstraction/eq9Distributeur2/Distributeur2.java
+++ src/abstraction/eq9Distributeur2/Distributeur2.java
@@ -12,11 +12,11 @@ public class Distributeur2 extends Distributeur2MarqueDistributeur  {
 	
 	public void next() {
 		super.next();
-		System.out.println(this.totalStocksChocoMarque.getValeur(Filiere.LA_FILIERE.getEtape(),cryptogramme));
-		System.out.println(this.getTotalStock(cryptogramme));
-		frais = this.totalStocksChocoMarque.getValeur(Filiere.LA_FILIERE.getEtape(),cryptogramme)*this.getCoutStockage();   // ajout des frais de stockage par maxime
+		//System.out.println(this.totalStocksChocoMarque.getValeur(Filiere.LA_FILIERE.getEtape(),cryptogramme));
+		//System.out.println(this.getTotalStock(cryptogramme));
+		frais = this.getTotalStock(cryptogramme)*this.getCoutStockage();   // ajout des frais de stockage par maxime
 		if (frais>0) {																										//On paye des frais énormes car la quantité du stock l'est aussi
-			Filiere.LA_FILIERE.getBanque().payerCout(this, cryptogramme, getDescription(), frais);}
+			Filiere.LA_FILIERE.getBanque().payerCout(this, cryptogramme,"frais de stockage", frais);}
 			this.getJournaux().get(0).ajouter("Ce coup si, nous avons payé : "+frais+" en coût de stockage : ");
 		}
 }
