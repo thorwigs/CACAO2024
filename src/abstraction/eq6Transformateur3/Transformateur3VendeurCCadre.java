861442a2919c2d778c114c70ca70b64d51c141ee
cchau <cchau@CE-DELL-IC>
2024-04-19 18:26:30 +0200

    somme plutot que moyenne

--- src/abstraction/eq6Transformateur3/Transformateur3VendeurCCadre.java
+++ src/abstraction/eq6Transformateur3/Transformateur3VendeurCCadre.java
@@ -111,17 +111,17 @@ public class Transformateur3VendeurCCadre extends Transformateur3AcheteurCCadre 
 	 * @author Thomas et Cédric
 	 */
 	public double propositionPrix(ExemplaireContratCadre contrat) {
-		double moyenne  = 0; 
+		double somme  = 0; 
 		int p = 0;
 		double prix = 0;
 		for (ExemplaireContratCadre contratCC : contratsEnCours) {
 			if (((Feve) contrat.getProduit()).equals((Feve) contratCC.getProduit())) {
-				moyenne += contratCC.getPrix();
+				somme += contratCC.getPrix();
 				p += 1;
 			
 			}
 		}
-		moyenne = moyenne/p;
+		double moyenne = somme/p;
 		prix = moyenne + 0.5 * 1200 + 8; // prise en compte du cout de production ( pas exactement car non prise en compte de la qualité de notre chocolat,0.5 choisi arbitrairementet(pourcentage d'adjuvants)) et du prix moyen de la tonne de fève qu'on achète dans nos contrats en cours
 		return 1.03 * prix;
 	}
