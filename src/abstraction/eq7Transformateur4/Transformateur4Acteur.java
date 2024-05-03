d8241941a77f0ac2916f4d9d3cdf9812e97d5c5d
pierr <pierr@172.16.136.142>
2024-05-03 10:52:42 +0200

    a

--- src/abstraction/eq7Transformateur4/Transformateur4Acteur.java
+++ src/abstraction/eq7Transformateur4/Transformateur4Acteur.java
@@ -118,8 +118,8 @@ public class Transformateur4Acteur implements IActeur, IFabricantChocolatDeMarqu
 		//on pourra rajouter d'autre chocolats que choco1 = mirage , sachant que mirage est le premier element de cette liste
 		//ici on parle directement du chocolat CocOasis on peut donc aposer notre marque
 		for (ChocolatDeMarque c : chocolatCocOasis) {
-			this.stockChocoMarque.put(c, 50000.0); //le premier element de stockchocomarque correspond a mirage
-			this.totalStocksChocoMarque.ajouter(this, 50000.0, cryptogramme);
+			this.stockChocoMarque.put(c, 45000.0); //le premier element de stockchocomarque correspond a mirage
+			this.totalStocksChocoMarque.ajouter(this, 45000.0, cryptogramme);
 			this.journal.ajouter(" stock("+ c +")->"+this.stockChocoMarque.get(c));
 		}
 		
