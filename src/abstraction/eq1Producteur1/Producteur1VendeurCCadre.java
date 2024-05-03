00cbef41b65effeba364d0f2d67ea97e79238568
Yuri69420 <146597745+Yuri69420@users.noreply.github.com>
2024-05-03 11:23:10 +0200

    IDK What is wrong

--- src/abstraction/eq1Producteur1/Producteur1VendeurCCadre.java
+++ src/abstraction/eq1Producteur1/Producteur1VendeurCCadre.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.util.LinkedList;
 import java.util.List;
 
-import abstraction.eqXRomu.acteurs.TransformateurX;
 import abstraction.eqXRomu.bourseCacao.BourseCacao;
 
 import abstraction.eqXRomu.contratsCadres.Echeancier;
@@ -43,16 +42,12 @@ public class Producteur1VendeurCCadre extends Producteur1VendeurBourse implement
 		super.next();
 		this.journalCC.ajouter("=== STEP "+Filiere.LA_FILIERE.getEtape()+" ====================");
 		for (Feve f : stock.keySet()) {
-			if (stock.get(f).getValeur()-restantDu(f)>100 ) { // au moins 100 tonnes par step pendant 6 mois
+			if (stock.get(f).getValeur()-restantDu(f)>1200 ) { // au moins 100 tonnes par step pendant 6 mois
 				this.journalCC.ajouter("   "+f+" suffisamment en stock pour passer un CC");
 				double parStep = Math.max(100, (stock.get(f).getValeur()-restantDu(f))/24); // au moins 100, et pas plus que la moitie de nos possibilites divisees par 2
 				Echeancier e = new Echeancier(Filiere.LA_FILIERE.getEtape()+1, 12, parStep);
 				List<IAcheteurContratCadre> acheteurs = supCC.getAcheteurs(f);
-				
-				acheteurs.remove(acheteurs.size()-1);
-				
 				if (acheteurs.size()>0) {
-					
 					IAcheteurContratCadre acheteur = acheteurs.get(Filiere.random.nextInt(acheteurs.size()));
 					journalCC.ajouter("   "+acheteur.getNom()+" retenu comme acheteur parmi "+acheteurs.size()+" acheteurs potentiels");
 					ExemplaireContratCadre contrat = supCC.demandeVendeur(acheteur, this, f, e, cryptogramme, false);
@@ -108,7 +103,17 @@ public class Producteur1VendeurCCadre extends Producteur1VendeurBourse implement
 		boolean bio =f.isBio();
 		boolean equitable = f.isEquitable();
 		double prime = 0;
-		
+		/*
+		if(gamme== Gamme.BQ) {
+			throw new IllegalArgumentException("Gamme BQ ne peut pas etre solde en Contrat Cadre.");
+		}
+		if (gamme == Gamme.MQ && !bio && !equitable ) {
+			throw new IllegalArgumentException("Gamme MQ sans label ne se vend pas en Contrat Cadre.");
+		}
+		if (gamme == Gamme.HQ && !bio && !equitable ) {
+			throw new IllegalArgumentException("Gamme HQ sans label ne se vend pas en Contrat Cadre.");
+		}
+		 */
 		if (bio) {
 			prime += 100;
 		}
@@ -142,7 +147,6 @@ public class Producteur1VendeurCCadre extends Producteur1VendeurBourse implement
 		Echeancier ec = contrat.getEcheancier();
 		IProduit produit = contrat.getProduit();
 		//Echeancier res = ec;
-		boolean accepted = false;
 		String type = produit.getType();
 		if (type != "Feve") {
 			journalCC.ajouter("Ce n'est pas une feve");
@@ -159,18 +163,18 @@ public class Producteur1VendeurCCadre extends Producteur1VendeurBourse implement
 			journalCC.ajouter("Pas de contract avec une duree inferieure a 5 mois");
 			return null;
 		}
-		if (Filiere.LA_FILIERE.getEtape() < 12) {
-			journalCC.ajouter("On fait pas de contract pendant les 6ers mois");
+		if (Filiere.LA_FILIERE.getEtape() < 24) {
+			journalCC.ajouter("On fait pas de contract pendant la 1ere annee");
 			return null;
 		}
-		if (this.contratsEnCours.size() >=3 ) {
+		if (this.contratsEnCours.size() <3 ) {
 			journalCC.ajouter("On fait pas plus que de 3 contracts en meme temps");
 			return null;
 		}
 		if (ec.getStepDebut()<Filiere.LA_FILIERE.getEtape()+8) {
-			accepted = true;
+			boolean Accepted = true;
 		}
-		if (accepted = false){
+		else {
 			if (ec.getQuantiteTotale()<=stock.get((Feve)produit).getValeur()-restantDu((Feve)produit)) {
 				journalCC.ajouter("      je retourne "+new Echeancier(Filiere.LA_FILIERE.getEtape()+1, 12,  (int)(ec.getQuantiteTotale()/12)));
 				return new Echeancier(Filiere.LA_FILIERE.getEtape()+1, 12,  (int)(ec.getQuantiteTotale()/12));
@@ -200,7 +204,7 @@ public class Producteur1VendeurCCadre extends Producteur1VendeurBourse implement
 	public double contrePropositionPrixVendeur(ExemplaireContratCadre contrat) {
 		// TODO Auto-generated method stub
 		List<Double> prix = contrat.getListePrix();
-		if (prix.get(prix.size()-1)>=0.95*prix.get(0)) {
+		if (prix.get(prix.size()-1)>=0.975*prix.get(0)) {
 			journalCC.ajouter("      contrePropose le prix demande : "+contrat.getPrix());
 			return contrat.getPrix();
 		} else {
