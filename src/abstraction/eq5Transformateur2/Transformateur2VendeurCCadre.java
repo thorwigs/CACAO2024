6fb2aa28a7781ce173bf0f2ff3cf0f8aef9bb41c
vince <vince@172.16.131.227>
2024-05-03 09:45:08 +0200

    Merge branch 'main' of https://github.com/R0000BIN/CACAO2024 into main

--- src/abstraction/eq5Transformateur2/Transformateur2VendeurCCadre.java
+++ src/abstraction/eq5Transformateur2/Transformateur2VendeurCCadre.java
@@ -138,7 +138,7 @@ public class Transformateur2VendeurCCadre extends Transformateur2AcheteurCCadre 
 	        return contrat.getPrix(); // ne refait pas de contreproposition
 	    } else {
 	    	EtapenegoVente++;
-	    	double renego = (contrat.getListePrix().get(EtapenegoVente - 1) - contrat.getListePrix().get(EtapenegoVente))*0.5; 
+	    	double renego = (contrat.getListePrix().get(EtapenegoVente - 1) - contrat.getListePrix().get(EtapenegoVente))*0.5; //renegocie le prix de 50% de la variation entre le prix proposé au tour précédent et la proposition de l'acheteur 
 	        return contrat.getPrix() + renego;
 	    }
 	}
