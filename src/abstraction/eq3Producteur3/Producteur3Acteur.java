2c66cfc70eaa01509432780f719ff51e9ae80f92
mammouYoussef <mammouYoussef@172.16.134.122>
2024-05-03 09:49:46 +0200

    salaires

--- src/abstraction/eq3Producteur3/Producteur3Acteur.java
+++ src/abstraction/eq3Producteur3/Producteur3Acteur.java
@@ -359,7 +359,7 @@ public abstract class Producteur3Acteur implements IActeur {
 
 		        // Déterminer le salaire en fonction du type de fève
 		        if (f.isEquitable()) {
-		            salaireOuvrier = 3.9 * 15; // Salaire pour l'equitable (bio ou non)
+		            salaireOuvrier = 3.9 * 15; // Salaire pour l'equitable (bio ou non) (*15 comme c'est 3.9 par jour et le step comporte 15 jours) 
 		        } else {
 		            salaireOuvrier = 2.6 * 15; // Salaire standard pour les non équitable 
 		        }
