48efc092fb55e3fd91342571733a43c0e6d75b6d
user <user@172.16.129.14>
2024-05-04 14:07:57 +0200

    nb

--- src/abstraction/eq6Transformateur3/Transformateur3.java
+++ src/abstraction/eq6Transformateur3/Transformateur3.java
@@ -1,11 +1,8 @@
 package abstraction.eq6Transformateur3;
 
-
-public class Transformateur3 extends Transformateur3VendeurAppelDOffre  {
+public class Transformateur3 extends Transformateur3Acteur  {
 	
 	public Transformateur3() {
 		super();
 	}
 }
-
-
