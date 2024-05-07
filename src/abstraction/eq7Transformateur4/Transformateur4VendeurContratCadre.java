95ff9d71d0c39d2ed792e0553e3c45caa50b9baa
pierr <pierr@172.16.136.142>
2024-05-07 12:03:39 +0200

    Merge branch 'main' of https://github.com/CACAO2024/CACAO2024.git into main

--- src/abstraction/eq7Transformateur4/Transformateur4VendeurContratCadre.java
+++ src/abstraction/eq7Transformateur4/Transformateur4VendeurContratCadre.java
@@ -192,7 +192,16 @@ public class Transformateur4VendeurContratCadre extends Transformateur4AcheteurC
 			pPrecedent = prixPrecedent.get(contrat);
 		}
 		double qte = contrat.getQuantiteTotale();
-		double coutProd = qte*(coutproduction_tonne_marque_step.get(contrat.getProduit())+ getPrixFèves(contrat.getProduit()));
+		
+		///////début//////modif 07/05 Pierrick
+		double coutProd = 0.0; //ce truc vaut la même chose que propositionPrix(contrat) mais sans la marge de x%
+		if (coutproduction_tonne_marque_step.isEmpty()){
+			coutProd = contrat.getQuantiteTotale()*(this.coutmachine + this.coutadjuvant*0.2 + getPrixFèves(contrat.getProduit())) + (1000*this.nbemployeCDI + 658)*0.1;
+		}
+		else {
+			coutProd = qte*(coutproduction_tonne_marque_step.get(contrat.getProduit()) + getPrixFèves(contrat.getProduit()));
+		}
+		//////fin/////////
 		
 		if (pPropose <  pPrecedent) {
 			if ((pPropose+pPrecedent)/2 >= coutProd*1.02){
