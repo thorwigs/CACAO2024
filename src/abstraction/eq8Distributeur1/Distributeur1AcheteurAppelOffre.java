c7e8e89832bdb92fc8e580008e0dfcb6a208b3a8
ianis <ianis@172.16.134.234>
2024-05-15 11:12:33 +0200

    rddftgyhu

--- src/abstraction/eq8Distributeur1/Distributeur1AcheteurAppelOffre.java
+++ src/abstraction/eq8Distributeur1/Distributeur1AcheteurAppelOffre.java
@@ -3,7 +3,9 @@ package abstraction.eq8Distributeur1;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
+import java.util.Iterator;
 import abstraction.eqXRomu.acteurs.Romu;
 import abstraction.eqXRomu.appelDOffre.IAcheteurAO;
 import abstraction.eqXRomu.appelDOffre.OffreVente;
@@ -64,7 +66,8 @@ public class Distributeur1AcheteurAppelOffre extends Distributeur1AcheteurContra
 			journalAO.ajouter(Romu.COLOR_LLGRAY, Romu.COLOR_LGREEN,"   refus de l'AO : pas assez d'argent sur le compte");
 			return null;
 		}
-		int choisi=-1; // permet de connaître la proposition choisi à la fin, la moins chere, ou renverra -1 si pas d'offre correspondante
+		
+		
 /*		int parcourir=0; //permet de parcourir la liste des propositions pour trouver la bonne
 		while(choisi==-1 && parcourir<propositions.size()) {
 			if (propositions.get(0).getOffre().getProduit().equals(propositions.get(parcourir).getProduit())!=true) {
@@ -75,39 +78,43 @@ public class Distributeur1AcheteurAppelOffre extends Distributeur1AcheteurContra
 			}  
 		} */
 		
+		int choisi=-1; // permet de connaître la proposition choisi à la fin, la moins chere, ou renverra -1 si pas d'offre correspondante
 		for (int i=0; i<propositions.size();i++) {
 			if(propositions.get(0).getOffre().getProduit().equals(propositions.get(i).getProduit())) {
 				this.choix.put(i,propositions.get(i));
 			}
 		}
 		
-		if (this.choix.size()==0) {
-			journalAO.ajouter(Romu.COLOR_LLGRAY, Romu.COLOR_LGREEN,"   refus de l'AO : produit pas correspondant à la demande");
-			return null;
-		} else if (this.choix.size() == 1){
-			choisi=0;
+        Set<Integer> keys = this.choix.keySet();
+        Iterator<Integer> iterator = keys.iterator();
+		
+		if (this.choix.size() == 1){
+			choisi = iterator.next();
 		} else if (this.choix.size()>1) {
-			double prix = propositions.get(0).getPrixT()*this.Coefficient.get(propositions.get(0).getVendeur().getNom());
+			double prix = this.choix.get(iterator.next()).getPrixT()*this.Coefficient.get(this.choix.get(iterator.next()).getVendeur().getNom());
 			int choix = 0;
-			for (int i=1; i<propositions.size();i++) {
-				if (propositions.get(i).getPrixT()*this.Coefficient.get(propositions.get(i).getVendeur().getNom())<prix) {
+			for (int i : keys) {
+				if (this.choix.get(i).getPrixT()*this.Coefficient.get(this.choix.get(i).getVendeur().getNom())<prix) {
 					choix = i;
-					prix = propositions.get(i).getPrixT();
+					prix = this.choix.get(i).getPrixT();
 				}
 			}
+			choisi=choix;
 		}
 		
 		
-		if ((solde<propositions.get(choisi).getPrixT()*propositions.get(choisi).getQuantiteT()))
-				 {
-			journalAO.ajouter(Romu.COLOR_LLGRAY, Romu.COLOR_LGREEN,"   refus de l'AO : pas assez d'argent sur le compte");
+		if (choisi==-1) {
+			journalAO.ajouter(Romu.COLOR_LLGRAY, Romu.COLOR_LGREEN,"   refus de l'AO : produit pas correspondant à la demande");
 			return null;
-				 }
-		else if (propositions.get(choisi).getPrixT()>super.prix_a_perte(propositions.get(choisi).getProduit(),super.prix((ChocolatDeMarque)(propositions.get(choisi).getProduit())) )) {
-			return null;
-		}
-		else {
-			return propositions.get(choisi);
+		} else {
+			if ((solde<propositions.get(choisi).getPrixT()*propositions.get(choisi).getQuantiteT())){
+				journalAO.ajouter(Romu.COLOR_LLGRAY, Romu.COLOR_LGREEN,"   refus de l'AO : pas assez d'argent sur le compte");
+				return null;
+			 } else if (propositions.get(choisi).getPrixT()>super.prix_a_perte(propositions.get(choisi).getProduit(),super.prix((ChocolatDeMarque)(propositions.get(choisi).getProduit())) )) {
+				 return null;
+			 } else {
+				 return propositions.get(choisi);
+			 }
 		}
 	}
 	
