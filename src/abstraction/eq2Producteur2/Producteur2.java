ab892f90791dca3663f184228a025e114be7043b
maxim <maxim@172.16.135.243>
2024-05-15 10:44:24 +0200

    Encheres

--- src/abstraction/eq2Producteur2/Producteur2.java
+++ src/abstraction/eq2Producteur2/Producteur2.java
@@ -2,7 +2,7 @@ package abstraction.eq2Producteur2;
 
 import abstraction.eqXRomu.produits.Feve;
 
-public class Producteur2 extends Producteur2VendeurCCadre {
+public class Producteur2 extends Producteur2VendeurEncheres {
 	public Producteur2() {
 		super();
 	}
