6aa64b074366b9424927a504fb22c05c2b0f6c64
Arthur <Arthur@172.16.132.167>
2024-05-22 11:57:10 +0200

    Merge branch 'main' of https://github.com/Pascalou31/CACAO2024.git into main

--- src/abstraction/eq6Transformateur3/Transformateur3VendeurCCadre.java
+++ src/abstraction/eq6Transformateur3/Transformateur3VendeurCCadre.java
@@ -128,9 +128,9 @@ public class Transformateur3VendeurCCadre extends Transformateur3Produit impleme
 		
 		Chocolat c = ((ChocolatDeMarque) contrat.getProduit()).getChocolat();
 		Feve f = super.Correspond(c);
-		double prix = coûtMoyenAchatFeve.get(f) + 0.5 * 1200 + 8; // prise en compte du cout de production ( pas exactement car non prise en compte de la qualité de notre chocolat,0.5 choisi arbitrairementet(pourcentage d'adjuvants)) et du prix moyen de la tonne de fève qu'on achète dans nos contrats en cours
+		double prix = coûtMoyenAchatFeve.get(f)*4.2 + 0.5 * 1200 + 8; // prise en compte du cout de production ( pas exactement car non prise en compte de la qualité de notre chocolat,0.5 choisi arbitrairementet(pourcentage d'adjuvants)) et du prix moyen de la tonne de fève qu'on achète dans nos contrats en cours
 		
-		return 1.03 * prix; //  un petit +3% pour maximiser le prfofit
+		return 1.03 * prix; //  un petit +3% pour maximiser le profit
 		
 	}
 	/**
