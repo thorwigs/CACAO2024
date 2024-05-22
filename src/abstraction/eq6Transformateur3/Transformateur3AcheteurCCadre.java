7e852a09d2538a936c61cb2e8ed7071d8c46da02
mahel <mahel@172.16.133.41>
2024-05-22 11:44:24 +0200

    on retire les print

--- src/abstraction/eq6Transformateur3/Transformateur3AcheteurCCadre.java
+++ src/abstraction/eq6Transformateur3/Transformateur3AcheteurCCadre.java
@@ -75,7 +75,6 @@ public class Transformateur3AcheteurCCadre extends PrévisionAide implements IAc
 		this.journalCC6.ajouter("=== Partie Achat fèves ====================");
 		HashMap<Feve, Integer> Decision = super.Decision();
 		super.addhistorique(coûtMoyenAchatFeve, stockChoco, stockChocoMarque);
-		System.out.println("Décision" + Decision);
 		for(Feve f : Decision.keySet()) {
 			if(Decision.get(f)>10) {
 				this.journalCC6.ajouter("   "+f+" suffisamment de vente pour passer un CC");
